30074489b981e0b72dbc8a6d18ed0df204ecec8c
Appshaker <appshaker@localhost>
2026-08-26 16:20:11 +0200

    fix: repair svelte.config.js after verification

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter()
+	}
+};
+
+export default config;
